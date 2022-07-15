e54b4a03c46a5393537000f6782a190a46adcef6
Lawrence Pina <lawrence@colada.com>
2022-07-14 20:53:06 -0700

    Initial Commit

---
+++ lisamarie/lisamarieApp.swift
@@ -0,0 +1,17 @@
+//
+//  lisamarieApp.swift
+//  lisamarie
+//
+//  Created by Lawrence Pina on 7/14/22.
+//
+
+import SwiftUI
+
+@main
+struct lisamarieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

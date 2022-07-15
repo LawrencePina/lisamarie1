3d2be7b435d0075a70d17355662637e2edbc948c
Lawrence Pina <lawrence@colada.com>
2022-07-14 21:18:55 -0700

    init

---
+++ lisamariewatch/lisamariewatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  lisamariewatchApp.swift
+//  lisamariewatch Watch App
+//
+//  Created by Colada LLC
+//
+
+import SwiftUI
+
+@main
+struct lisamariewatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
